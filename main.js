4332c1c6bc0ab85098ad26be6ff357a1e610865c
KarissaSingh <89837891+KarissaSingh@users.noreply.github.com>
2022-06-17 17:45:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-video ="";
+video = "";
+objects = [];
+Status = "";
 
 function preload() {
     video = createVideo('video.mp4');
@@ -6,16 +8,50 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(480,380);
+    canvas = createCanvas(480, 380);
     canvas.center();
 }
 
 function draw() {
-    image(video,0,0,480,380);
+    image(video, 0, 0, 480, 380);
+    if (Status != "") {
+        Od.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are " + objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (objects[i].label == object_name) {
+                video.stop();
+                Od.detect(gotResult);
+                document.getElementById("object_status").innerHTML = object_name + " Found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+                synth.speak(utterThis);
+            }
+            else {
+                document.getElementById("object_status").innerHTML = object_name + " Not Found";
+            }
+        }
+    }
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() {
-    Od=ml5.objectDetector('cocossd',modelLoaded);
+    Od = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Object";
 }
 
